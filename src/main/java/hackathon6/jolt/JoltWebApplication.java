e6ec09ac5eb0f25cfedaffa93a59ef1606736742
Naresh Sharma <naresh.sharma@globallogic.com>
2018-09-12 12:13:14 +0530

    initial commit

---
+++ src/main/java/hackathon6/jolt/JoltWebApplication.java
@@ -0,0 +1,12 @@
+package hackathon6.jolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoltWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoltWebApplication.class, args);
+	}
+}
---
+++ src/test/java/hackathon6/jolt/JoltWebApplicationTests.java
@@ -0,0 +1,16 @@
+package hackathon6.jolt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoltWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
